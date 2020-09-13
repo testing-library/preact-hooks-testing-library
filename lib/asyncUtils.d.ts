451d6909bd69b31cdaa7484f1312acf9e6dcc38f
Andrew Duthie <andrew@andrewduthie.com>
2020-09-13 13:39:00 -0400

    Sync to @testing-library/react-hooks@3.4.1

--- lib/asyncUtils.d.ts
+++ lib/asyncUtils.d.ts
@@ -1,11 +1,13 @@
 import { ResolverType } from "./_types";
 export interface TimeoutOptions {
     timeout?: number;
+    interval?: number;
     suppressErrors?: boolean;
 }
 declare function asyncUtils(addResolver: (resolver: ResolverType) => void): {
-    waitForValueToChange: (selector: () => any, options?: TimeoutOptions) => Promise<void>;
-    waitForNextUpdate: (options?: TimeoutOptions) => Promise<void>;
     wait: (callback: () => any, options?: TimeoutOptions) => Promise<void>;
+    waitFor: (callback: () => any, { interval, timeout, suppressErrors }?: TimeoutOptions) => Promise<void>;
+    waitForNextUpdate: (options?: TimeoutOptions) => Promise<void>;
+    waitForValueToChange: (selector: () => any, options?: TimeoutOptions) => Promise<void>;
 };
 export default asyncUtils;
