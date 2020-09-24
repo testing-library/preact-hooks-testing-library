55529d1f80a9a36ad6a64bf94133bf85eaabd7ac
Frederik Bring <frederik.bring@trivago.com>
2020-09-24 16:02:06 +0200

    Prevent falsy return values from being overwritten

--- test/customHook.test.ts
+++ test/customHook.test.ts
@@ -1,29 +1,69 @@
 import { useState, useCallback } from "preact/hooks";
 import { renderHook, act } from "../src";
 
-describe("custom hook tests", () => {
-  function useCounter() {
-    const [count, setCount] = useState(0);
+describe("Custom hooks", () => {
+  describe("useCounter", () => {
+    function useCounter() {
+      const [count, setCount] = useState(0);
 
-    const increment = useCallback(() => setCount(count + 1), [count]);
-    const decrement = useCallback(() => setCount(count - 1), [count]);
+      const increment = useCallback(() => setCount(count + 1), [count]);
+      const decrement = useCallback(() => setCount(count - 1), [count]);
 
-    return { count, increment, decrement };
-  }
+      return { count, increment, decrement };
+    }
 
-  test("should increment counter", () => {
-    const { result } = renderHook(() => useCounter());
+    test("should increment counter", () => {
+      const { result } = renderHook(() => useCounter());
 
-    act(() => result.current.increment());
+      act(() => result.current.increment());
 
-    expect(result.current.count).toBe(1);
+      expect(result.current.count).toBe(1);
+    });
+
+    test("should decrement counter", () => {
+      const { result } = renderHook(() => useCounter());
+
+      act(() => result.current.decrement());
+
+      expect(result.current.count).toBe(-1);
+    });
   });
 
-  test("should decrement counter", () => {
-    const { result } = renderHook(() => useCounter());
+  describe("return proper fasly values", () => {
+    type Falsy = 0 | null | undefined | false | "" | undefined;
 
-    act(() => result.current.decrement());
+    function useFalsy(value: Falsy) {
+      return value;
+    }
 
-    expect(result.current.count).toBe(-1);
+    test("`false`", () => {
+      const { result } = renderHook(() => useFalsy(false));
+
+      expect(result.current).toBe(false);
+    });
+
+    test("`0`", () => {
+      const { result } = renderHook(() => useFalsy(0));
+
+      expect(result.current).toBe(0);
+    });
+
+    test("`null`", () => {
+      const { result } = renderHook(() => useFalsy(null));
+
+      expect(result.current).toBe(null);
+    });
+
+    test("`''`", () => {
+      const { result } = renderHook(() => useFalsy(""));
+
+      expect(result.current).toBe("");
+    });
+
+    test("`undefined`", () => {
+      const { result } = renderHook(() => useFalsy(undefined));
+
+      expect(result.current).toBe(undefined);
+    });
   });
 });
